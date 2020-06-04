b13383f038e0532b71eae1f0dc2418b40370926b
Gabriel Fernandes <gabrielfernnd@gmail.com>
2020-06-04 18:33:37 -0300

    api: add dependencies

--- api/server.js
+++ api/server.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
+  host: 'localhost',
+  dialect: 'postgres'
+})
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.')
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err)
+  })
 
 app.use(express.static('../app/build'))
 
