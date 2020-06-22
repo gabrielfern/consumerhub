851483cc80a3ea86c54c44daec55620a1e5d51d9
Gabriel Fernandes <gabrielfernnd@gmail.com>
2020-06-22 14:34:56 -0300

    app: fix display of user image
    Part of #3.

--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -1,3 +1,4 @@
+/* global fetch */
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
@@ -26,7 +27,10 @@ export default () => {
       </div>
       <div>
         <br />
-        <img src={`/api/users/${id}/image`} alt='imagem de usuário' />
+        <img
+          src={`/api/users/${id}/image`} alt='imagem de usuário'
+          style={{ display: 'block', maxWidth: '100%' }}
+        />
       </div>
     </>
   )
