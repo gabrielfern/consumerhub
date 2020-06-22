acf2eb34eb9f9d564627f3d8e27fce1b36cf5418
Gabriel Fernandes <gabrielfernnd@gmail.com>
2020-06-21 22:19:31 -0300

    api: serve index.html as fallback to permit client side routing

--- api/server.js
+++ api/server.js
@@ -1,12 +1,18 @@
+const path = require('path')
 const express = require('express')
 const usersRouter = require('./routes/users')
+const rootPath = path.dirname(__dirname)
+const buildPath = path.join(rootPath, 'app', 'build')
 const app = express()
 const port = process.env.PORT || 8000
 
 app.use(express.json())
-app.use(express.static('../app/build'))
-
+app.use(express.static(buildPath))
 app.use('/api/users', usersRouter)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Running on http://localhost:${port}`)
