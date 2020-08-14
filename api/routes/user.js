13e488731f27d98e9adc2ab6eda7b257b6ea5e54
Gabriel Fernandes <gabrielfernnd@gmail.com>
2020-08-14 19:37:06 -0300

    api: merge friends' routes into user router

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { Sequelize, Friendship } = require('../models')
 const { auth, createUserToken, checkPassword } = require('./auth')
 const { wrap } = require('../utils/errorHandlers')
 
@@ -47,6 +48,64 @@ router.post('/image', wrap(async (req, res) => {
     image: req.body.length ? req.body : null
   })
   res.end()
+}))
+
+router.get('/friends', wrap(async (req, res) => {
+  res.send(await req.user.getFriends())
+}))
+
+router.post('/friends', wrap(async (req, res) => {
+  if (req.user.id !== req.body.user &&
+    !await req.user.getFriendshipWith(req.body.user)) {
+    const friendship = await Friendship.create({
+      accepted: false,
+      userId1: req.user.id,
+      userId2: req.body.user
+    })
+    res.send(friendship)
+  } else {
+    res.status(400).end()
+  }
+}))
+
+router.put('/friends', wrap(async (req, res) => {
+  const result = await Friendship.update({
+    accepted: true
+  }, {
+    where: {
+      userId1: req.body.user,
+      userId2: req.user.id
+    }
+  })
+  if (result[0]) {
+    res.end()
+  } else {
+    res.status(404).end()
+  }
+}))
+
+router.delete('/friends', wrap(async (req, res) => {
+  const result = await Friendship.destroy({
+    where: {
+      [Sequelize.Op.or]: [{
+        userId1: req.user.id,
+        userId2: req.body.user
+      }, {
+        userId1: req.body.user,
+        userId2: req.user.id
+      }]
+    }
+  })
+  if (result) {
+    res.end()
+  } else {
+    res.status(404).end()
+  }
+}))
+
+router.get('/friends/invitations', wrap(async (req, res) => {
+  const friendships = await req.user.getFriendships()
+  res.send(friendships.filter(friendship => !friendship.accepted))
 }))
 
 module.exports = router
